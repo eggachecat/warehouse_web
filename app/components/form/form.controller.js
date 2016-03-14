76fc1696303641dd8dbb8e3c19ae3dc4a8dfca82
sunao <sunao_0626@hotmail.com>
2016-03-14 12:36:35 +0800

    redesign form
    add barcode area

--- app/components/form/form.controller.js
+++ app/components/form/form.controller.js
@@ -5,6 +5,12 @@ app.controller('FormCtrl', ['$scope', 'toastService',function($scope, toastServi
 	};
 	
 	$scope.editor = user.id;
+	$scope.good = {}
+
+	$scope.generateBarcode = function(){
+		$scope.good.barcode = 123123121;
+		toastService.showSimpleToast("Generate Barcode" + $scope.good.barcode, 30000);
+	}
 
 	$scope.onSubmit = function(){
 		toastService.showSimpleToast("Generate Hash Code", 30000);
